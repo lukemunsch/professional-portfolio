61cddc389dea5e07c302f78385291ce5a27a1512
Luke Munsch <lpmunsch@gmail.com>
2023-02-23 20:37:59 +0000

    Create migrations for new model in resume

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Project
+
+# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    """set up how to view models in admin"""
+    list_display = (
+        'friendly_proj_name',
+        'languages',
+    )
+    ordering = ('project_name',)
+    search_fields = ['project_name', 'languages',]
+
+
+admin.site.register(Project, ProjectAdmin)
--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,53 @@
+"""set up our models"""
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    """Set up model for resume projects"""
+    project_name = models.CharField(
+        max_length=30,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    proj_desc = models.TextField()
+    proj_qr = models.ImageField(null=True, blank=True)
+    screengrab = models.ImageField(
+        null=True,
+        blank=True
+    )
+    screengrab_2 = models.ImageField(
+        null=True,
+        blank=True
+    )
+    languages = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    live_site = models.CharField(
+        max_length=100,
+        default='',
+        null=False,
+        blank=False
+    )
+    github_repo = models.CharField(
+        max_length=150,
+        default='',
+        null=True,
+        blank=True
+    )
+    friendly_proj_name = models.CharField(
+        max_length=30,
+        default='',
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        """return string value"""
+        return self.project_name
+
+    def get_friendly_district_name(self):
+        """set up the friendly versions of our district names"""
+        return self.friendly_district_name
