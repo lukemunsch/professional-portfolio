0a7dd02e9c3112011c379fb8935d9115d76e61f0
Luke Munsch <lpmunsch@gmail.com>
2023-02-23 20:43:46 +0000

    Update file content for gh-repo page

--- ghrepo/views.py
+++ ghrepo/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def github_repo(request):
     """set up our index view"""
-    return render(request, 'ghrepo/github-repo.html')
+    return render(request, 'ghrepo/gh-repo.html')
