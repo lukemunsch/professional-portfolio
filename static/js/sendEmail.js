3dc8cf82d69bcb8037120148d2ca4d12019fd3d5
Luke Munsch <lpmunsch@gmail.com>
2023-03-03 15:25:50 +0000

    Update content form to fix response issue

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,6 +1,7 @@
 function sendMail(contactForm) {
     console.log("sending...");
     emailjs.send("gmail", "portfolio-template", {
+            "to_name": "Luke",
             "from_name": contactForm.name.value,
             "from_number": contactForm.contactnumber.value,
             "from_email": contactForm.emailaddress.value,
