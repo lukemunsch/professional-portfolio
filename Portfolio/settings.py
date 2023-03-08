5543d0e6d836c0d6ec3450958cf669bb7e00acb2
Luke Munsch <lpmunsch@gmail.com>
2023-03-08 21:42:48 +0000

    Fix spelling mistake in aws settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -160,7 +160,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGES_BUCKET_NAME = 'lukemunsch-professional-portfolio'
+    AWS_STORAGE_BUCKET_NAME = 'lukemunsch-professional-portfolio'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
